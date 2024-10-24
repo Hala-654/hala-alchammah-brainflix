d5c69c7766bd41103fa03fb84904b40eb794c079
Hala Alchammah <hala.alchammah@gmail.com>
2024-10-23 20:56:22 -0400

    header initial  javascript complete

---
+++ src/components/sections/header/header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import brainflixLogo from '../../../assets/Logo/BrainFlix-logo.svg';
+import searchIcon from '../../../assets/Icons/search.svg';
+import uploadIcon from '../../../assets/Icons/upload.svg';
+import mohanAvatar from '../../../assets/images/Mohan-muruge.jpg';
+
+import 'src/components/sections/header/header.scss'
+
+
+function header() {
+    return (
+        <header className="header">
+            <img src={brainflixLogo} alt="BrainFlix Logo" className="header__logo"/>
+            <div className="header-nav">
+                <input className="header-nav__search" placeholder="search"></input>
+                <img className="header-nav__search--icon" src={searchIcon} alt="search icon" />
+            </div>
+            <a className="header-nav__btn" href="">
+                <img src={uploadIcon} alt="upload icon"/>
+                upload
+            </a>
+            <div className="header-nav__avatar">
+                <img src={mohanAvatar} alt="Mohan Avatar"/>
+            </div>
+        </header>
+    )
+}
+export default header
