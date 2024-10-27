0689ce1e2c81fee5789dd4b5605fb3194de8022e
Hala Alchammah <hala.alchammah@gmail.com>
2024-10-27 11:05:48 -0400

    Fix body styling

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -12,15 +12,17 @@ function Body() {
           <h3>By Aiden Thompson</h3>
           <p>8/8/2023</p>
         </div>
-        <div className="body__icon">
-          <img src={views} alt="views icon" />
-          <img src={likes} alt="likes icon" />
+        <div className="body__author--icon">
+          <p><img src={views} alt="views icon" />
+           980,000</p>
+          <p><img src={likes} alt="likes icon" />
+           22,479</p>
         </div>
       </div>
 
       {/* line */}
       <div className="body__text">
-        <p>
+        <span>
           Explore the cutting-edge developments and predictions for Artificial
           Intelligence in the coming years. From revolutionary breakthroughs in
           machine learning to the ethical considerations influencing AI
@@ -28,7 +30,7 @@ function Body() {
           speculation. Join us on a journey that navigates the intricate
           interplay between innovation, ethics, and the ever-evolving tech
           frontier.
-        </p>
+        </span>
         <h3>3 Comments</h3>
       </div>
     </div>
