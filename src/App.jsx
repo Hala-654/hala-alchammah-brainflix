01ad22b800dfbbe85c3486c39f4dbe289da96386
Hala Alchammah <hala.alchammah@gmail.com>
2024-10-26 23:58:55 -0400

    create main video jsx and sass file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
 import "./App.scss";
+import Video from "./components/Video/Video";
 
 function App() {
   return (
     <>
       <Header />
+      <Video />
     </>
-  );
+  )
 }
 
 export default App;
--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,10 +1,44 @@
 import "../Video/Video.scss";
-
+import uploadVideo from "../../assets/images/Upload-video-preview.jpg";
+import play from "../../assets/Icons/play.svg";
+import volume from "../../assets/Icons/volume_up.svg";
+import fullscreen from "../../assets/Icons/fullscreen.svg";
 
 function Video() {
-    return (
-<p></p>
-    );
+  const handlePlay = () => {
+    console.log("Play button was clicked");
+  };
+  const handleVolume = () => {
+    console.log("Volume button was clicked");
+  };
+  const handleFullscreen = () => {
+    console.log("Fullscreen button was clicked");
+  };
+
+  return (
+    <div className="video-container">
+      <video poster={uploadVideo} className="video" />
+      <div className="controls">
+        <div className="controls-left">
+          <button className="controls-left__play" onClick={handlePlay}>
+            <img src={play} alt="play icon" />
+          </button>
+        </div>
+
+        <div className="controls-right">
+          <button
+            className="controls-right__fullscreen"
+            onClick={handleFullscreen}
+          >
+            <img src={fullscreen} alt="fullscreen icon" />
+          </button>
+          <button className="controls-right__volumn" onClick={handleVolume}>
+            <img src={volume} alt="volumn icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Video;
