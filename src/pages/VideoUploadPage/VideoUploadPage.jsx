4365ea03c39ecf6dcadf23610528046f6ca8813a
Hala Alchammah <hala.alchammah@gmail.com>
2024-11-01 19:31:46 -0400

    work on upload page buttons

--- src/pages/VideoUploadPage/VideoUploadPage.jsx
+++ src/pages/VideoUploadPage/VideoUploadPage.jsx
@@ -1,14 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "../VideoUploadPage/VideoUploadPage.scss";
 import thumbnail from "../../assets/images/Upload-video-preview.jpg";
 import publish from "../../assets/Icons/publish.svg";
 
 function VideoUploadPage() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+
+    if (!title || !description) {
+      alert("please fill out both fields!");
+      return;
+    }
+    alert("You have successfully uploaded your video!");
+    navigate("/");
+  };
+
+  const handleCanel = () => {
+    setTitle("");
+    setDescription("");
+    navigate("/");
+  };
   return (
     <>
       <div className="upload">
         <h1 className="upload__title">Upload Video</h1>
-        <form className="upload__form--wrapper">
+        <form onSubmit={handleSubmit} className="upload__form--wrapper">
           <div className="Upload__form">
             <p className="upload__thumbnail">VIDEO THUMBNAIL</p>
             <img
@@ -25,6 +47,8 @@ function VideoUploadPage() {
                 rows="2"
                 placeholder="Add a title to your video"
                 spellCheck="false"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               ></textarea>
               <p className="upload__video--description">
                 ADD A VIDEO DESCRIPTION
@@ -36,21 +60,27 @@ function VideoUploadPage() {
                 rows="6"
                 placeholder="Add a description to your video"
                 spellCheck="true"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               ></textarea>
             </div>
           </div>
           <div className="upload__button">
-            <Link to={`/`}>
-              <button type="button" className="upload__button--publish">
-                <img
-                  src={publish}
-                  alt="publish icon"
-                  className="upload__img--publish"
-                />
-                <p>PUBLISH</p>
-              </button>
-            </Link>
-            <button type="button" className="upload__button--cancel">
+            {/* <Link to={`/`}> */}
+            <button type="submit" className="upload__button--publish">
+              <img
+                src={publish}
+                alt="publish icon"
+                className="upload__img--publish"
+              />
+              <p>PUBLISH</p>
+            </button>
+            {/* </Link> */}
+            <button
+              type="button"
+              className="upload__button--cancel"
+              onClick={handleCanel}
+            >
               CANCEL
             </button>
           </div>
