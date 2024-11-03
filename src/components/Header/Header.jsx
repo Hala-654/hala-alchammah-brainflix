a0926feb159430b41d0fad7b77e08d1a60b8d4d4
Hala Alchammah <hala.alchammah@gmail.com>
2024-11-02 22:22:20 -0400

    edit Header for tablet display

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,18 @@ import searchIcon from "../../assets/Icons/search.svg";
 import uploadIcon from "../../assets/Icons/upload.svg";
 import mohanAvatar from "../../assets/images/Mohan-muruge.jpg";
 import { Link } from "react-router-dom";
-import VideoUploadPage from "../../pages/VideoUploadPage/VideoUploadPage";
 
 import "../Header/Header.scss";
 
 function Header() {
   return (
     <header className="header">
-      <a className="header__link" href="/">
-        <img src={brainflixLogo} alt="BrainFlix Logo" />
-      </a>
+      <div className="header__logo">
+        <a className="header__link" href="/">
+          <img src={brainflixLogo} alt="BrainFlix Logo" />
+        </a>
+      </div>
+
       <div className="header__search--section">
         <div className="header__search--container">
           <img
@@ -24,25 +26,37 @@ function Header() {
             className="header__search--bar"
             type="text"
             placeholder="Search"
-          ></input>
+          />
         </div>
-
-        <img className="header__avatar" src={mohanAvatar} alt="Mohan Avatar" />
+        <div>
+          <img
+            className="header__mobile--avatar"
+            src={mohanAvatar}
+            alt="Mohan Avatar"
+          />
+        </div>
       </div>
 
+      <Link to={`/upload`} className="header__tablet">
+        <button className="header__tablet--button">
+          <img
+            className="header__tablet--button__icon"
+            src={uploadIcon}
+            alt="upload icon"
+          />
+          <p>UPLOAD</p>
+        </button>
+      </Link>
+
       <div>
-        <Link to={`/upload`}>
-          <button className="header__button">
-            <img
-              className="header__button--icon"
-              src={uploadIcon}
-              alt="upload icon"
-            />
-            <p>UPLOAD</p>
-          </button>
-        </Link>
+        <img
+          className="header__tablet--avatar"
+          src={mohanAvatar}
+          alt="Mohan Avatar"
+        />
       </div>
     </header>
   );
 }
+
 export default Header;
